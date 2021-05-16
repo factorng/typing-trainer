bcf2bf0ac3e93ba5e21faa8dc12b76004b621c9d
Aleksey Efimov <factorng@yandex.ru>
2021-05-16 13:43:19 +0300

    chars count added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from "react";
 import getText from "./utils/api";
+import currentTime from "./utils/currentTime";
 import useKeyPress from "./hooks/useKeyPress";
 
 function App() {
   const [text, setText] = React.useState("");
   const [currentChar, setCurrentChar] = React.useState(null);
+  const [mistakeChar, setMistakeChar] = React.useState(false);
+  const [startTime, setStartTime] = React.useState(null);
+  const [charsCount, setCharsCount] = React.useState(0);
 
   useKeyPress((key) => {
+    if (!startTime) {
+      setStartTime(currentTime());
+    }
+    setMistakeChar(false);
+    if (currentChar === text.length - 1) {
+      alert("succes");
+    }
     if (key == text.charAt(currentChar)) {
-      console.log("right");
       setCurrentChar(currentChar + 1);
+      setCharsCount(charsCount + 1);
     } else {
-      console.log("wrong");
+      setMistakeChar(true);
     }
   });
   React.useEffect(() => {
     getText()
       .then((res) => {
-        setText(res);
+        setText(res.substring(0, 40));
         setCurrentChar(0);
       })
       .catch((err) => console.log(err)); // work with errors
@@ -26,7 +37,34 @@ function App() {
     <>
       <div>
         <span style={{ color: "green" }}>{text.substring(0, currentChar)}</span>
-        <spam>{text.substring(currentChar, text.length)}</spam>
+        {mistakeChar ? (
+          <span
+            style={{
+              color: "red",
+              backgroundColor: "lightgrey",
+              border: "1px solid black",
+            }}
+          >
+            {text.charAt(currentChar)}
+          </span>
+        ) : (
+          <span
+            style={{
+              color: "purple",
+              backgroundColor: "lightgrey",
+              border: "1px solid black",
+            }}
+          >
+            {text.charAt(currentChar)}
+          </span>
+        )}
+        <spam>{text.substring(currentChar + 1, text.length)}</spam>
+      </div>
+      <div>
+        <span>Chars per second </span>
+        <span>
+          {(charsCount / ((currentTime() - startTime) / 60000)).toFixed(0)}
+        </span>
       </div>
     </>
   );
